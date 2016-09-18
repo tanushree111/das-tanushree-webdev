defcfce71c9eb70db9c2ad9b60b895772a0cb09c
Tanushree Das <das.ta@husky.neu.edu>
2016-09-18 15:04:23 -0400

    mongo db url added

--- test/app.js
+++ test/app.js
@@ -4,7 +4,7 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://tan:tan111@ds033046.mlab.com:33046/webdevtan';
 
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
